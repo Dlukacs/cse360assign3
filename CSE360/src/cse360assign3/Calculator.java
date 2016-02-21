64080b66688b242e1d08d84965c7e9c62cf5d1c0
David Lukacs <dlukacs@asu.edu>
2016-02-20 18:38:05 -0700

    Phase 1 J-Unit testing implemented.

--- CSE360/src/cse360assign3/Calculator.java
+++ CSE360/src/cse360assign3/Calculator.java
@@ -4,38 +4,71 @@
  * CSE 360
  * Assignment 3
  * 
+ * @author David Lukacs
+ * 
  */
 
 package cse360assign3;
 
 public class Calculator {
-
+	
 	private int total;
 	
+	/**
+	 * Calculator constructor: 
+	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+
+	/**
+	 * getTotal method: Returning correct total sum.
+	 * @return Returns total value. 
+	 */
 	public int getTotal () {
 		return 0;
 	}
 	
+	/**
+	 * add method: adds value.
+	 * @param value
+	 */
 	public void add (int value) {
 		
 	}
 	
+	/**
+	 * subtract method: subtracts parameter from total value. 
+	 * @param value
+	 */
 	public void subtract (int value) {
 		
 	}
 	
+	/**
+	 * multiply method: multiplies total value by parameter value. 
+	 * @param value
+	 */
 	public void multiply (int value) {
 		
 	}
 	
+	
+	/**
+	 * divide method: divides total value by parameter value. 
+	 * Use integer division.  If the parameter is zero, set the total to zero.  
+	 * Do not print an error message.
+	 * 
+	 * @param value
+	 */
 	public void divide (int value) {
 		
 	}
 	
+	/**
+	 * getHistory method: returns an empty string.
+	 */
 	public String getHistory () {
 		return "";
 	}
---
+++ CSE360/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,55 @@
+/**
+ * David Lukacs
+ * Pin# 518
+ * CSE 360
+ * Assignment 3
+ * J-Unit testing
+ * 
+ * 
+ * @author David Lukacs
+ */
+
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testCalculator() {
+		Calculator cal = new Calculator();
+		assert (cal) != null;
+	}
+	
+	@Test
+	public void testGetTotal(){
+		fail("Noy yet implemented");
+	}
+	
+	@Test
+	public void testAdd(){
+		fail("Noy yet implemented");
+	}
+	
+	@Test
+	public void testSubtract(){
+		fail("Noy yet implemented");
+	}
+
+	@Test
+	public void testMultiply(){
+		fail("Noy yet implemented");
+	}
+	
+	@Test
+	public void testDivide(){
+		fail("Noy yet implemented");
+	}
+
+	@Test
+	public void testGetHistory(){
+		fail("Noy yet implemented");
+	}
+}
